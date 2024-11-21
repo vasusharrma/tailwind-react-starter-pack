638578309ee3600abd4b3ef34e2050630128ea5c
vasusharrma <sharmavashu099@gmail.com>
2024-11-21 18:31:54 +0530

    up to date

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './index.css'
+
+
+
+
 const App = () => {
+
+
+
   return (
+    
     <>
       <div className='flex min-h-screen space-y-1 flex-col items-center justify-center'>
         <h2 className='text-gray-500 text-2xl'>React-Tailwind Starter Pack</h2>
         <h1 className='text-4xl font-extrabold'>write "npm start" or  "npx parcel index.html" to run the app</h1>
       </div>
     </>
+
   );
 };
 
